15a20c2fcd01d984e1d8e6ed62c904a328ec2403
John Gunter <john_gunter_77@yahoo.com>
2019-12-30 21:39:56 -0800

    Adding Users.js

---
+++ src/Users.js
@@ -0,0 +1,30 @@
+import * as data from './users.json';
+
+export class Users extends React.Component {
+    constructor() {
+        this.users = {};
+        this.userNames = [];
+        this.dataProcessed = false;
+    }
+
+    processData = async () => {
+        data.forEach(element => {
+            if (this.users[element['id']] === undefined) {
+                this.users[element['id']] = {}
+            }
+            
+            this.users[element['user_id']]['name'] = element['name']
+            this.users[element['user_id']]['occupation'] = element['occupation']
+            this.users[element['user_id']]['avatar'] = element['avatar']
+        })
+    }
+
+    // This checks if data has processed and sets the flag to true
+    startProcessing = async() => {
+        this.processData().then(() => this.dataProcessed = true)
+    }
+
+    getRecoard = (user_id) => {
+       return this.users[user_id] !== undefined ? this.users[user_id] : null
+    }
+}
